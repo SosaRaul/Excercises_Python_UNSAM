519cd98b03393646c8ef0b49701cf466ff46c9ea
RaulRSosa <tecraulsosa@gmail.com>
2021-09-08 08:49:54 -0300

    Ej5.2 solved

---
+++ Clase05/cocumpleaños.py
@@ -0,0 +1,15 @@
+# Ejercicio 5.3
+def cumple_aleatorio():
+    # Bug en febrero 30 y 31
+    dia = random.choices([dia for dia in range(1:32)])
+    mes = random.choices([dia for dia in range(1:13)])
+    return dia,mes
+
+def es_cocumpleaños(dia,mes):
+    return 
+    
+
+
+def prob_cocumpleaños(n):
+
+# TERMINAR!
